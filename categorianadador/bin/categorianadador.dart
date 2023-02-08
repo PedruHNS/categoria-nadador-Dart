6e2b69db208982d2426a97c8693a6452d291e25a
Pedro Siqueira <pedro.siqueira1999@hotmail.com>
2023-02-07 21:57:03 -0300

    função de leitura do usuário criada

--- categorianadador/bin/categorianadador.dart
+++ categorianadador/bin/categorianadador.dart
@@ -1,5 +1,19 @@
 import 'package:categorianadador/funcoes.dart';
 
-void main() {
-  print("");
+
+// 8. Faça um procedimento que recebe a idade de um nadador por parâmetro e
+//retorna , também por parâmetro, a categoria desse nadador de acordo com a
+//tabela abaixo:
+
+// Idade	Categoria
+// 5 a 7 anos	Infantil A
+// 8 a 10 anos	Infantil B
+// 11-13 anos	Juvenil A
+// 14-17 anos	Juvenil B
+// Maiores de 18 anos (inclusive)	Adulto
+void main(List<String> arguments) {
+  final nome = leitura(mensagem: "nome do aluno");
+  final idade = int.parse(leitura(mensagem: "idade do aluno"));
+
+  
 }
